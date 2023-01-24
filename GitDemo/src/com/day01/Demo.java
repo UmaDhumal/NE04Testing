71d50231adf7741f5ba928d1087d779211adc70a
UmaDhumal <123440175+UmaDhumal@users.noreply.github.com>
2023-01-24 12:56:18 +0530

    Update Demo.java

--- GitDemo/src/com/day01/Demo.java
+++ GitDemo/src/com/day01/Demo.java
@@ -2,6 +2,11 @@ package com.day01;
 
 public class Demo 
 {
+	
+	public void m1()
+	{
+		System.out.println("Hello world");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Demo Git");
