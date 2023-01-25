e6a2837caaa5786393dcf0e17485ef722a7c4ea5
admin <admin@DESKTOP-6099UR4>
2023-01-25 11:36:07 +0530

    by xyz

---
+++ GitDemo/src/com/day01/Demo01.java
@@ -0,0 +1,11 @@
+package com.day01;
+
+public class Demo01 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Demo 01 Method");
+		
+	}
+
+}
---
+++ GitProject01/src/com/cjc01/A.java
@@ -0,0 +1,11 @@
+package com.cjc01;
+
+public class A 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("A class");
+		
+	}
+
+}
