8ff02b8122bb7d1b6e9fa6d1bc62f7e5870a790a
UmaDhumal <123440175+UmaDhumal@users.noreply.github.com>
2023-01-26 18:19:31 +0530

    by Shobha

--- GitProject01/src/com/cjc01/A.java
+++ GitProject01/src/com/cjc01/A.java
@@ -2,6 +2,10 @@ package com.cjc01;
 
 public class A 
 {
+	public void m1()
+	{
+		System.out.println("m1 from Shobha");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("A class");
